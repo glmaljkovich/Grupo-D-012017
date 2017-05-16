03f38077a5309ff6a994009d99bbbda7fe0bea95
Gabriel <gabriel.l.maljkovich@gmail.com>
2017-05-16 13:59:07 -0300

    Creado CashRegisterController
    sin implementar

---
+++ src/main/java/grupod/desapp/unq/edu/ar/services/CashRegisterController.java
@@ -0,0 +1,14 @@
+package grupod.desapp.unq.edu.ar.services;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by gabriel on 16/05/17.
+ */
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/checkout")
+public class CashRegisterController {
+}
