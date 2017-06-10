d2fd0f42e53fab4f30e6971de165d09fb1873abe
Gabriel <gabriel.l.maljkovich@gmail.com>
2017-06-10 17:47:38 -0300

    Refactor Authentication Service

--- src/main/java/grupod/desapp/unq/edu/ar/security/TokenAuthenticationService.java
+++ src/main/java/grupod/desapp/unq/edu/ar/security/TokenAuthenticationService.java
@@ -5,9 +5,11 @@ import io.jsonwebtoken.Claims;
 import io.jsonwebtoken.Jws;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.security.authentication.AuthenticationCredentialsNotFoundException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.stereotype.Service;
 
@@ -24,7 +26,7 @@ import java.util.List;
 public class TokenAuthenticationService {
     static final long EXPIRATIONTIME = 864_000_000; // 10 days
     static final String SECRET = "totallynot1234";
-    static final String TOKEN_PREFIX = "Bearer";
+    static final String TOKEN_PREFIX = "Bearer ";
     static final String HEADER_STRING = "Authorization";
 
     public static void addAuthentication(HttpServletRequest req, HttpServletResponse res, Authentication auth) {
@@ -34,7 +36,7 @@ public class TokenAuthenticationService {
                 .claim("accessLevel", auth.getAuthorities().stream().findFirst().get().getAuthority()+"_ROLE")
                 .setIssuedAt(new Date())
                 .compact();
-        res.addHeader(HEADER_STRING, TOKEN_PREFIX + " " + JWT);
+        res.addHeader(HEADER_STRING, TOKEN_PREFIX + JWT);
         req.setAttribute("token", JWT);
 
     }
@@ -50,23 +52,24 @@ public class TokenAuthenticationService {
 
     public static Authentication getAuthentication(HttpServletRequest request) {
         String token = request.getHeader(HEADER_STRING);
-        if (token != null) {
+
+        if (token == null || !token.startsWith("Bearer "))
+            throw new AuthenticationCredentialsNotFoundException("No JWT token found in request headers");
             // parse the token.
 
-            Jws<Claims> jwt = Jwts.parser()
-                           .setSigningKey(SECRET)
-                           .parseClaimsJws(token.replace(TOKEN_PREFIX, ""));
+        Jws<Claims> jwt = Jwts.parser()
+                       .setSigningKey(SECRET)
+                       .parseClaimsJws(token.replace(TOKEN_PREFIX, ""));
 
-            String user = jwt.getBody().getSubject();
-            String accessLevel = jwt.getBody().get("accessLevel", String.class);
 
-            List<GrantedAuthority> authorityList = new ArrayList<>();
-            authorityList.add(new SimpleGrantedAuthority(accessLevel+"_ROLE"));
+        String user = jwt.getBody().getSubject();
+        String accessLevel = jwt.getBody().get("accessLevel", String.class);
 
-            return user != null ?
-                    new UsernamePasswordAuthenticationToken(user, null, authorityList) :
-                    null;
-        }
-        return null;
+        List<GrantedAuthority> authorityList = AuthorityUtils.commaSeparatedStringToAuthorityList(accessLevel);
+
+        return user != null ?
+                new UsernamePasswordAuthenticationToken(user, null, authorityList) :
+                null;
+
     }
 }
